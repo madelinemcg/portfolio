acff0c37b7c6a1b9eed9e51e53f19dfda5e1d828
McGinnis <Madeline.McGinnis@biworldwide.com>
2022-08-22 14:15:32 -0500

    Animation fixes, mail, etc

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,17 +3,26 @@ import Profile from "../../assets/images/profile.png";
 import Arrow from "../../assets/images/arrow.png";
 import { COPY } from "../../assets/copy/copy";
 import gsap from "gsap";
+import { useEffect } from "react";
+import { Button } from "@mui/material";
 
 export const HomePage: React.FC = () => {
-  gsap.to(".title", { duration: 0.01, opacity: 0 });
-  gsap.to(".title", { duration: 3, opacity: 1 });
+  useEffect(() => {
+    gsap.to(".title", { duration: 0.01, opacity: 0 });
+    gsap.to(".title", { duration: 2.5, opacity: 1 });
+  });
+
+  useEffect(() => {
+    gsap.to(".profile-wrapper", { duration: 0.01, opacity: 0, x: -160 });
+    gsap.to(".profile-wrapper", { duration: 1.5, opacity: 1, x: 0 });
+  });
 
   return (
     <div className="page">
       <div className="background" />
       <div className="content">
         <div className="profile">
-          <img src={Profile} alt="" className="img-wrapper" />
+          <img src={Profile} alt="" className="profile-wrapper" />
           <div className="left-text">
             <div className="heading">
               <h2 className="title">{COPY.PROFILE.HEADING}</h2>
@@ -22,6 +31,11 @@ export const HomePage: React.FC = () => {
             <div>{COPY.PROFILE.DESCRIPTION}</div>
             <div className="sub-text">{COPY.PROFILE.SUB_DESCRIPTION}</div>
           </div>
+        </div>
+        <div className="button-wrapper">
+          <Button href={""} className="button">
+            Resume
+          </Button>
         </div>
       </div>
     </div>
