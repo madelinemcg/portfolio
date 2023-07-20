262dd266039d7f05f4fc20d8a35603bb8e4de5de
mcgin374 <mcgin374@umn.edu>
2023-07-20 15:11:14 -0500

    space issue fix

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -30,10 +30,11 @@ export const HomePage: React.FC = () => {
             </div>
             <div>{COPY.PROFILE.DESCRIPTION}</div>
             <div className="sub-text">{COPY.PROFILE.SUB_DESCRIPTION}</div>
+            <div className="space"></div>
             <div className="heading">
               <h2 className="title">{COPY.EDUCATION.HEADING}</h2>
             </div>
-            <div className="sub-text">{COPY.EDUCATION.UMN.HEADING}</div>
+            <div>{COPY.EDUCATION.UMN.HEADING}</div>
             <div className="small-text">{COPY.EDUCATION.UMN.DESCRIPTION}</div>
             <div className="space"></div>
             <div className="sub-text">{COPY.EDUCATION.NORM.HEADING}</div>
