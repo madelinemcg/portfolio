ac221171e8e0b12ecb3731d43a85242b3eff1b95
mcgin374 <mcgin374@umn.edu>
2023-07-20 14:57:13 -0500

    home education page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -32,12 +32,12 @@ export const HomePage: React.FC = () => {
             <div className="sub-text">{COPY.PROFILE.SUB_DESCRIPTION}</div>
             <div className="heading">
               <h2 className="title">{COPY.EDUCATION.HEADING}</h2>
-              <div className="sub-text">{COPY.EDUCATION.UMN.HEADING}</div>
-              <div className="sub-text">{COPY.EDUCATION.UMN.DESCRIPTION}</div>
-              <div></div>
-              <div className="sub-text">{COPY.EDUCATION.NORM.HEADING}</div>
-              <div className="sub-text">{COPY.EDUCATION.NORM.DESCRIPTION}</div>
             </div>
+            <div className="sub-text">{COPY.EDUCATION.UMN.HEADING}</div>
+            <div className="sub-text">{COPY.EDUCATION.UMN.DESCRIPTION}</div>
+            <div></div>
+            <div className="sub-text">{COPY.EDUCATION.NORM.HEADING}</div>
+            <div className="sub-text">{COPY.EDUCATION.NORM.DESCRIPTION}</div>
           </div>
         </div>
         <div className="button-wrapper">
